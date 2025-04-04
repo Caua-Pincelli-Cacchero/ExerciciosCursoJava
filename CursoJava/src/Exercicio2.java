113c4bccca4b10defff46f52970d0619d2d466bf
Cauã Pincelli Cacchero <cauapincacchero@gmail.com>
2025-04-04 17:13:38 -0300

    Exercicio2

---
+++ CursoJava/src/Exercicio2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Locale lingua = Locale.getDefault();
+        System.out.println("A lingua do seu computador é " + lingua);
+    }
+}
